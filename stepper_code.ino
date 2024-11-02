a13fa35345164b220b645cc180bb6ade2d875447
arshia2007 <guptaarshia1@gmail.com>
2024-11-02 20:26:19 +0530

    Update stepper_code.ino

--- stepper_code.ino
+++ stepper_code.ino
@@ -1,25 +1,51 @@
 int dir_pin;
+int step_pin;
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(step_pin, OUTPUT);
   pinMode(dir_pin, OUTPUT);
+  digitalWrite(dir_pin, LOW);
 }
 
 void loop() {
   Serial.println("Enter data: ");
 
-  while (Serial.available() == 0) {
-  }
-
+  if (Serial.available() > 0) {
+  
+// 10w100 (10 - Revolutions, w-direction, 100 - speed)
   String input = Serial.readString();
-  String speed = input.substring(1);
-  int n = speed.toInt();
 
-  if (input[0] == 'w') {
-    digitalWrite(dir_pin, HIGH);
-    delay(n);
-  } else if (input[0] == 's') {
-    digitalWrite(dir_pin, LOW);
-    delay(n);
+  int speed = input.substring(3).toInt();
+  char direction = input[2];
+  int rev = input.substring(0,2).toInt();
+
+  Serial.println("Speed: ");
+  Serial.println(speed);
+
+  Serial.println("No. of revolutions: ");
+  Serial.println(rev);
+
+  for (int i = 0; i < 200*rev; i++){
+    if (direction == 'w'){
+      Serial.println("Direction: Forward");
+      digitalWrite(dir_pin, HIGH); //forward dir
+      digitalWrite(step_pin, HIGH);
+      delay(speed);
+      digitalWrite(step_pin, LOW);
+      delay(speed);
+    } else if (direction == 's'){
+      Serial.println("Direction: Backward");
+      digitalWrite(dir_pin, LOW); //backward dir
+      digitalWrite(step_pin, HIGH);
+      delay(speed);
+      digitalWrite(step_pin, LOW);
+      delay(speed);
+    } else{
+      Serial.println("Invalid Input");
+    }
+
+  }
   }
 }
